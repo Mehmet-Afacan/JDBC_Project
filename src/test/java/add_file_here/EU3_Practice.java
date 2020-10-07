216e6bac864364b91e56d9953c9feb86e761e486
Mehmet-Afacan <mehmetafacan42@hotmail.com>
2020-10-07 17:42:30 +0100

    Super Commit

---
+++ src/test/java/add_file_here/EU3_Practice.java
@@ -0,0 +1,19 @@
+package add_file_here;
+
+public class EU3_Practice {
+
+    public static void main(String[] args) {
+
+        System.out.println("EU3 is opening pull request");
+        //commit your change
+        //with nice commit message
+        //push it back to the github
+        //observe the change
+        //from your github repo
+        //click on compare
+        //create a pul request
+        //to asl original repo owner
+        //to merge your change
+
+    }
+}
