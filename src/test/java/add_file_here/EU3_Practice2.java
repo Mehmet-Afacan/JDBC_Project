e453418f75955d1f3d520b050229e77421feb25b
Mehmet-Afacan <mehmetafacan42@hotmail.com>
2020-11-27 14:07:19 +0000

    Second study initial commit

---
+++ src/test/java/add_file_here/EU3_Practice2.java
@@ -0,0 +1,12 @@
+package add_file_here;
+
+public class EU3_Practice2 {
+
+    public static void main(String[] args) {
+        System.out.println("One more time adding something for practice");
+
+        //commit your change
+        //with nice commit message
+        //push it back to the github
+    }
+}
