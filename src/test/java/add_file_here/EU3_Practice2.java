4ce6aee1fd56683ddbfbb0fbfe05e1a2f578720a
Mehmet-Afacan <mehmetafacan42@hotmail.com>
2020-11-27 14:23:54 +0000

    Some changes in gitignore

--- src/test/java/add_file_here/EU3_Practice2.java
+++ src/test/java/add_file_here/EU3_Practice2.java
@@ -8,5 +8,12 @@ public class EU3_Practice2 {
         //commit your change
         //with nice commit message
         //push it back to the github
+
+        //observe the change
+        //from your github repo
+        //click on compare
+        //create a pul request
+        //to asl original repo owner
+        //to merge your change
     }
 }
